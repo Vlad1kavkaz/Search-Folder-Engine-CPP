016b2621a09b3629eccde903e65e722801bf0a46
Vlad1kavkaz <162821820v@gmail.com>
2023-09-02 23:20:16 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1,53 +1,37 @@
+
 #include <iostream>
-#include <string>
-#include <curl/curl.h>
+#include <filesystem>
+#include <fstream>
 
-// Функция обратного вызова Curl для получения ответа API
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
-    size_t totalSize = size * nmemb;
-    output->append((char*)contents, totalSize);
-    return totalSize;
-}
+namespace fs = std::filesystem;
 
-// Функция для отправки GET запросов к API
-std::string MakeRequest(const std::string& url) {
-    CURL* curl = curl_easy_init();
-    std::string response;
+void searchRepositories(const std::string& searchTerm) {
+    std::string path = "<путь к корневому каталогу репозиториев>"; // Замените на ваш путь
 
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    for (const auto& dirEntry : fs::recursive_directory_iterator(path)) {
+        if (dirEntry.is_regular_file()) {
+            std::ifstream file(dirEntry.path());
+            std::string line;
+            int lineNumber = 1;
 
-        CURLcode res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            std::cerr << "Failed to perform curl request: " << curl_easy_strerror(res) << std::endl;
+            while (std::getline(file, line)) {
+                if (line.find(searchTerm) != std::string::npos) {
+                    std::cout << dirEntry.path() << ": " << lineNumber << ": " << line << std::endl;
+                }
+                lineNumber++;
+            }
+
+            file.close();
         }
-
-        curl_easy_cleanup(curl);
     }
-
-    return response;
 }
 
 int main() {
-    // Ваш персональный токен доступа
-    std::string accessToken = "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";
+    std::string searchTerm;
+    std::cout << "Введите слово для поиска: ";
+    std::cin >> searchTerm;
 
-    // Ваш запрос поиска
-    std::string searchQuery = "web+development";
-
-    // Формирование URL для выполнения поиска на GitHub API
-    std::string apiUrl = "https://api.github.com/search/repositories?q=" + searchQuery;
-    std::string fullUrl = apiUrl + "&access_token=" + accessToken;
-
-    // Отправка запроса и получение ответа API
-    std::string response = MakeRequest(fullUrl);
-
-    // Обработка ответа API и извлечение информации о репозиториях
-    // Отобразите результаты поиска перед пользователем
-    std::cout << "Search results: " << std::endl;
-    std::cout << response << std::endl;
+    searchRepositories(searchTerm);
 
     return 0;
 }
